e435494e9469e3e637e9bc662ce202e00c2c2c39
Vitaly Kurennov <vkurennov@gmail.com>
2014-01-25 15:32:46 +0600

    Remove custom actions from ProductsController

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,16 +5,6 @@ class Admin::ProductsController < Admin::BaseController
   before_action :build_resource, only: [:index, :new]
   before_action :collection, only: [:create, :destroy], if: -> { request.xhr? }
 
-  def create
-    create! do |success, failure|
-      success.js { flash[:notice] = 'Js Ok!' }
-      failure.js { flash[:notice] = 'Js Fail!' }
-    end
-  end
-
-  def destroy
-    destroy! { root_path }
-  end
 
   private
 
