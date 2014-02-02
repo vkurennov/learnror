3210cecfd00d65615d8fc07990515c9e3b61da82
Vitaly Kurennov <vkurennov@gmail.com>
2014-02-02 18:51:10 +0600

    PrivatePub variant 1

--- app/assets/javascripts/lots.js.coffee
+++ app/assets/javascripts/lots.js.coffee
@@ -1,4 +1,1 @@
-$ ->
-  PrivatePub.subscribe '/lots/update', (data, channel) ->
-    console.log(data)
-    $('#lot_' + data.lot_id + ' .end_time').html(data.end_time)
+
--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -3,13 +3,4 @@ class BetsController < InheritedResources::Base
 
   respond_to :js
   actions :create
-
-  def create
-    create! do |success, failure|
-      success.js do
-        PrivatePub.publish_to '/lots/update', lot_id: resource.lot.id, end_time: resource.lot.end_time.to_s
-        render nothing: true
-      end
-    end
-  end
 end
