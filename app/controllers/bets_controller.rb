aad1c3a833170e0981d99feabcb904fbf8ec459c
Vitaly Kurennov <vkurennov@gmail.com>
2014-02-02 18:44:00 +0600

    Small Fix

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -8,6 +8,7 @@ class BetsController < InheritedResources::Base
     create! do |success, failure|
       success.js do
         PrivatePub.publish_to '/lots/update', lot_id: resource.lot.id, end_time: resource.lot.end_time.to_s
+        render nothing: true
       end
     end
   end
