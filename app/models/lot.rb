4a9cba2322fa58eaa4350a1441efee210a381c14
Vitaly Kurennov <vkurennov@gmail.com>
2014-02-12 23:42:41 +0600

    Lot

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -2,6 +2,8 @@ class Lot < ActiveRecord::Base
   has_many :bets
 
   def increase_price
+    self.price += 1
+    self.save!
   end
 
   def increase_end_time
--- spec/models/lot_spec.rb
+++ spec/models/lot_spec.rb
@@ -1,9 +1,24 @@
 require 'spec_helper'
 
 describe Lot do
+  let(:lot) { Lot.new }
+
+  describe "#increase_price" do
+    it 'increases price' do
+      expect { lot.increase_price }.to change(lot, :price).by(1)
+    end
+
+    it 'saves changed price' do
+      expect(lot.price).to eq(0)
+      lot.increase_price
+      lot.reload
+      expect(lot.price).to eq(1)
+    end
+  end
+
   it '#increase_price' do
     pending
-  end
+   end
 
   it '#increase_end_time' do
     pending
